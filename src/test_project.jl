bddd3f7e8f4cc5691b21163cef5b5b584ce9887b
paul raynaud <paul.raynaud66@hotmail.fr>
2019-10-09 17:04:58 +0200

    premier commit avec test

--- src/test_project.jl
+++ src/test_project.jl
@@ -2,4 +2,9 @@ module test_project
 
 greet() = print("Hello World!")
 
+
+test_2() = 2 
+
+export greet, test_2
+
 end # module
---
+++ test/runtests.jl
@@ -0,0 +1,4 @@
+using Test, test_project
+
+@test greet() == "Hello World!"
+@test test_2() == 2
