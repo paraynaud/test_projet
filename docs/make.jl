0aaf030ed6aa98e13a1b74db3d43b641a47cc923
paul raynaud <paul.raynaud66@hotmail.fr>
2019-10-10 11:54:44 +0200

    modif du readme docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,9 @@ using Documenter
 include("../src/test_project.jl")
 using .test_project
 
-makedocs(modules = [test_project], sitename="My Documentation")
+makedocs(modules = [test_project],
+	 sitename="test_projet",
+	 format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+	)
 
 deploydocs(repo = "github.com/paraynaud/test_projet.git")
